5d400b0304920fe64f974bf7c06744e6e2b91ffd
Rafael Mariano <toruticas@gmail.com>
2020-12-16 16:41:07 -0300

    No mocks example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import useSWR, { SWRConfig } from 'swr';
 
-function App() {
+const fetchArticles = async () => {
+  const response = await fetch('https://run.mocky.io/v3/ed8bc812-7198-4eb5-b07e-6fcc74de90f5');
+  const { data } = await response.json();
+
+  return data;
+};
+
+const Articles = () => {
+  const { data: articles } = useSWR('@articles', fetchArticles);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      {articles.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ol>
   );
-}
+};
+
+const App = () => {
+  return (
+    <SWRConfig
+      value={{
+        suspense: true,
+      }}
+    >
+      <div>
+        <h1>List of articles</h1>
+        <Suspense fallback={<p>Loading Articles...</p>}>
+          <Articles />
+        </Suspense>
+      </div>
+    </SWRConfig>
+  );
+};
 
 export default App;
