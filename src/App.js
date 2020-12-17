e43709343237874de43547a84f6a20dc1a95a397
Rafael Mariano <toruticas@gmail.com>
2020-12-16 22:16:44 -0300

    MSW browser example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { Suspense } from 'react';
 import useSWR, { SWRConfig } from 'swr';
 
-const { NODE_ENV } = process.env;
-const IS_DEVELOPMENT = NODE_ENV === 'development';
-
 const fetchArticles = async () => {
-  let json;
-  if (IS_DEVELOPMENT) {
-    const module = await import('./articles');
-    json = module.default;
-  } else {
-    const response = await fetch('https://run.mocky.io/v3/ed8bc812-7198-4eb5-b07e-6fcc74de90f5');
-    json = await response.json();
-  }
+  const response = await fetch('https://my-app.dev/articles');
+  const json = await response.json();
 
   return json.data;
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const { NODE_ENV } = process.env;
+const IS_DEVELOPMENT = NODE_ENV === 'development';
+
+if (IS_DEVELOPMENT) {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
