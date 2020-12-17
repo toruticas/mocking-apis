69d667d603be0211585ab619945f30718b62efa8
Rafael Mariano <toruticas@gmail.com>
2020-12-16 21:06:19 -0300

    Mock with json example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Suspense } from 'react';
 import useSWR, { SWRConfig } from 'swr';
 
-const fetchArticles = async () => {
-  const response = await fetch('https://run.mocky.io/v3/ed8bc812-7198-4eb5-b07e-6fcc74de90f5');
-  const { data } = await response.json();
+const { NODE_ENV } = process.env;
+const IS_DEVELOPMENT = NODE_ENV === 'development';
 
-  return data;
+const fetchArticles = async () => {
+  let json;
+  if (IS_DEVELOPMENT) {
+    const module = await import('./articles');
+    json = module.default;
+  } else {
+    const response = await fetch('https://run.mocky.io/v3/ed8bc812-7198-4eb5-b07e-6fcc74de90f5');
+    json = await response.json();
+  }
+
+  return json.data;
 };
 
 const Articles = () => {
